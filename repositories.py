2fa0f76266a95130d44e48b16dc880b1c5e92388
Christopher Brown <io@henrian.com>
2013-06-23 18:34:42 -0500

    Create FileSequence interface, which should work mostly like a file. Not tested.

--- repositories.py
+++ repositories.py
@@ -8,15 +8,81 @@ filename = sys.argv[1]
 
 headers = dict(Authorization='token %s' % os.environ['GITHUB_TOKEN'])
 
-line = None
-if os.path.exists(filename):
-    with open(filename, 'r') as repos_in:
-        for line in repos_in:
-            pass
 
-since = json.loads(line)['id'] if line else 0
+class FileSequence(object):
+    # breaks only on newlines
 
-with open(filename, 'a') as repos_out:
+    # indexes that come up in filenames start at 1
+    index = 1
+
+    # each file will contain no more than `limit` bytes
+    limit = 5e7
+
+    # the current file that's open
+    current_file = None
+    current_bytes = 0
+
+    def __init__(self, pattern, flag='w'):
+        if flag == 'r' or '+' in flag:
+            raise NotImplemented('FileSequence is currently write-only')
+
+        self.pattern = pattern
+        self.flag = flag
+
+        if flag('a'):
+            # seek to the end of the data
+            # we want index to land on the last existing file
+            for index in range(1000):
+                # e.g.: a-01.txt a-02.txt a-03.txt a-04.txt
+                if not os.path.exists(self.pattern % index):
+                    break
+            else:
+                raise Exception('Cannot write more than 1000 files')
+            # e.g.: index is now = 5, so we backpedal to point at the last existing file
+            self.index = index - 1
+
+    def __enter__(self):
+        self.next()
+
+    def __exit__(self):
+        self.current_file.close()
+
+    def next(self):
+        # close the current file if there is one
+        if self.current_file:
+            self.current_file.close()
+        self.current_file = open(self.current_path, self.flag)
+        self.current_bytes = 0
+        self.index += 1
+
+    def write(self, line):
+        if self.current_bytes + len(line) > self.limit:
+            # flush current file and start new one
+            self.next()
+
+        self.current_file.write(line)
+        self.current_bytes += len(line)
+
+    @property
+    def current_path(self):
+        return self.pattern % self.index
+
+    def tail(self, n=10):
+        if os.path.exists(self.current_path):
+            return [line for line in open(self.current_path, 'r')][-n:]
+        else:
+            return []
+
+
+file_sequence = FileSequence('respositories-%02d.json', 'a')
+
+# sequence.tail() may very well return an empty list
+entries = [dict(id=0)] + [json.loads(line) for line in file_sequence.tail()]
+
+# since = ['id'] if line else 0
+since = entries[-1]['id']
+
+with file_sequence as output:
     while True:
         r = requests.get('https://api.github.com/repositories', headers=headers, params=dict(since=since))
         entries = r.json()
@@ -44,5 +110,6 @@ with open(filename, 'a') as repos_out:
                 'owner_id': owner['id'],
                 'owner_type': owner['type'],
             }
-            repos_out.write('%s\n' % json.dumps(repo))
+            output.write('%s\n' % json.dumps(repo))
+
         since = entries[-1]['id']
