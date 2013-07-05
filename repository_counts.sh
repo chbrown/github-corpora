c30a987018e42517989cf0c57a3f6da675db2b0f
Christopher Brown <io@henrian.com>
2013-07-05 13:58:58 -0500

    More repos

--- repository_counts.sh
+++ repository_counts.sh
@@ -1,8 +1,8 @@
 echo capitals
-<repositories.json json -C name | grep '[A-Z]' | wc -l
+cat repositories*.json json -C name | grep '[A-Z]' | wc -l
 echo underscores
-<repositories.json json -C name | grep _ | wc -l
+cat repositories*.json json -C name | grep _ | wc -l
 echo hyphens
-<repositories.json json -C name | grep - | wc -l
+cat repositories*.json json -C name | grep - | wc -l
 echo total
-<repositories.json wc -l
+cat repositories*.json wc -l
