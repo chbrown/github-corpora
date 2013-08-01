040e6016be6b77ff17c269723488d65bf0f1844c
Christopher Brown <io@henrian.com>
2013-08-01 18:06:32 -0500

    Refactor filesequence stuff to pypi package

--- README.md
+++ README.md
@@ -3,13 +3,13 @@
 Scripts for crawling the GitHub API and resulting data (which is all public).
 
     export GITHUB_TOKEN=YTaSQhwUqb5gyNKCOXfveplH4GVcEsLxBjW0rnio
-    python repositories.py repositories.json
+    python repositories.py
 
-Objects in `repositories.json` are a slight modification of what you get from API v3 /repositories,
+Objects in `data/repositories-*.json` are a slight modification of what you get from API v3 /repositories,
 mostly deleting the redundant urls that can be predicted, presumably, given the `full_name` field.
 
     # count number of repos with capitals in the name
-    <repositories.json json -C name | grep [A-Z] | wc -l
+    cat data/repositories-*.json json -C name | grep [A-Z] | wc -l
 
     # count some other stuff like that
     repository_counts.sh
--- repositories.py
+++ repositories.py
@@ -1,91 +1,21 @@
 import os
-import sys
 import json
 import requests
 import httplib
+import filesequence
 
 
 headers = dict(Authorization='token %s' % os.environ['GITHUB_TOKEN'])
 
+filenames = filesequence.interpolator('data/repositories-%02d.json', xrange(1, 100))
 
-class FileSequence(object):
-    # breaks only on newlines
+with filesequence.open(filenames, 'a+') as output:
+    # sequence.tail() may very well return an empty list
+    entries = [dict(id=0)] + [json.loads(line) for line in output.tail()]
 
-    # indexes that come up in filenames start at 1
-    index = 1
+    # since = ['id'] if line else 0
+    since = entries[-1]['id']
 
-    # each file will contain no more than `limit` bytes
-    limit = 5e7
-
-    # the current file that's open
-    current_file = None
-    current_bytes = 0
-
-    def __init__(self, pattern, flag='w'):
-        if flag == 'r' or '+' in flag:
-            raise NotImplemented('FileSequence is currently write-only')
-
-        self.pattern = pattern
-        self.flag = flag
-
-        if flag == 'a':
-            # seek to the end of the data
-            # we want index to land on the last existing file
-            for index in range(1, 1000):
-                # e.g.: a-01.txt a-02.txt a-03.txt a-04.txt
-                if not os.path.exists(self.pattern % index):
-                    break
-            else:
-                raise Exception('Cannot write more than 1000 files')
-            # e.g.: index is now = 5, so we backpedal to point at the last existing file
-            self.index = index - 1
-
-    def __enter__(self):
-        self.next()
-        return self
-
-    def __exit__(self, type, value, traceback):
-        self.current_file.close()
-
-    def next(self):
-        # close the current file if there is one
-        if self.current_file:
-            self.current_file.close()
-        self.current_file = open(self.current_path, self.flag)
-        self.current_bytes = self.current_file.tell()
-        self.index += 1
-
-    def write(self, line):
-        if self.current_bytes + len(line) > self.limit:
-            # flush current file and start new one
-            self.next()
-
-        self.current_file.write(line)
-        self.current_bytes += len(line)
-
-    @property
-    def current_path(self):
-        return self.pattern % self.index
-
-    def tail(self, n=10):
-        if os.path.exists(self.current_path):
-            return [line for line in open(self.current_path, 'r')][-n:]
-        else:
-            return []
-
-
-file_sequence = FileSequence('repositories-%02d.json', 'a')
-
-# sequence.tail() may very well return an empty list
-entries = [dict(id=0)] + [json.loads(line) for line in file_sequence.tail()]
-
-# since = ['id'] if line else 0
-since = entries[-1]['id']
-
-# print since
-# exit()
-
-with file_sequence as output:
     while True:
         try:
             r = requests.get('https://api.github.com/repositories', headers=headers, params=dict(since=since))
